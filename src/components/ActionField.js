d0fd82dcc23b0d5a944c3c5bc7135e0963374d47
Charles Jr Ausejo <charlesjrausejo@gmail.com>
2021-09-10 14:42:07 +0800

    Add pushing of routes

--- src/components/ActionField.js
+++ src/components/ActionField.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import testData from '../data/test_data.json';
 import { Avatar, Paper } from '@material-ui/core';
 import './ActionField.css';
+
+
 
 function ActionField() {
     const [riderIndex,setRiderIndex] = useState(0);
@@ -13,9 +15,48 @@ function ActionField() {
     const [driverPhoto,setDriverPhoto] = useState("");
     const [riderName,setRiderName] = useState("None");
     const [driverName,setDriverName] = useState("None");
+    const [routeList,setRouteList] = useState([]);
+    const [index,setIndex] = useState(0);
+    const [hasRoute,setHasRoute] = useState(false);
+
     const driversList = testData.driversList;
     const ridersList = testData.ridersList;
 
+    useEffect(()=> {
+        console.log(routeList);
+    },[routeList]);
+    useEffect(()=> {
+
+    },[index])
+    async function pushData(url){
+        await axios.get(url);
+    }
+    const manualPush = async (e) => {
+        var lat = routeList[index][0];
+        var lng = routeList[index][1];
+        console.log(lat);
+        console.log(lng);
+        var url = 'http://localhost:5001/kapiot-46cbc/us-central1/setRoute?d=' + driverIndex.toString() + '&lat=' + lat.toString() + '&lng=' + lng.toString();
+        pushData(url);
+        setIndex(index+1);
+    }
+    const startPush = async (e) => {
+        e.preventDefault();
+        for(var i = 0; i < routeList.length; i ++){
+            var lat = routeList[i][0];
+            var lng = routeList[i][1];
+            console.log(lat);
+            console.log(lng);
+            var url = 'http://localhost:5001/kapiot-46cbc/us-central1/setRoute?d=' + driverIndex.toString() + '&lat=' + lat.toString() + '&lng=' + lng.toString();
+            setTimeout(pushData,3000,url);
+        }
+    }
+    const setRoute = async (e) =>  {
+        e.preventDefault();
+        const foundRoute = await axios.get("http://localhost:5001/kapiot-46cbc/us-central1/getRoute?d="+driverIndex.toString());
+        setRouteList(foundRoute.data);
+        setHasRoute(true);
+    }
     const populateAll = (e) => {
         e.preventDefault();
         axios.get("http://localhost:5001/kapiot-46cbc/us-central1/populateAll");
@@ -91,6 +132,12 @@ function ActionField() {
                 <Button onClick={acceptRider}variant='contained' color='primary' className="action__button">Accept Rider</Button>
                 <Button onClick={dropRider} variant='contained' color='primary' className="action__button">Drop Rider</Button>
                 <Button onClick={populateAll} variant='contained' color='primary' className="action__button">Populate Firebase</Button>
+                <Button onClick={setRoute} variant='contained' color='primary' className="action__button">Set Route</Button>
+                {hasRoute && <> 
+                    <Button onClick={startPush} variant='contained' color='primary' className="action__button">Start Route Push</Button>
+                    <Button onClick={manualPush} variant='contained' color='primary' className="action__button">Manual Route Push</Button>
+                    </>
+                }
             </div>
         </div>
         <Paper className="actionField__right">
